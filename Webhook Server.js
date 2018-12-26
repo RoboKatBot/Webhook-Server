b4520a8f2f6cde5194f59fcb0f3497cb2a799bd3
unknown <Robokatbot@gmail.com>
2018-12-26 12:06:06 +0800

    Initial Commit

---
+++ Webhook Server.js
@@ -0,0 +1,61 @@
+let RssFeedEmitter = require('rss-feed-emitter');
+let feeder = new RssFeedEmitter();
+const https = require('https');
+const {exec} = require('child_process');
+
+feeder.add({
+	url: 'https://xkcd.com/rss.xml',
+	refresh: 2000
+});
+
+feeder.add({
+	url:"http://feeds.feedburner.com/Explosm",
+	refresh:2000
+});
+
+feeder.on('new-item',(item)=>{
+	if (item.date < Date.now()-86400000) return;
+	send({content:item.link});
+});
+
+
+
+function send(payload) {
+	https.request({
+		host:"discordapp.com",
+		path:"/api/webhooks/367102978403991554/Lk6DN4QksDhe4oUxOd0TWH-AYJGt7ZrUcSpgHzmQlgc57CgugSBuNts0qmFoCy1Cj2sz",
+		method:"POST",
+		headers:{
+			"Content-Type":"multipart/form-data"
+		}
+	}).end(JSON.stringify(payload));
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+const fs = require('fs');
+const pfx = require('fs').readFileSync('./mycert.pfx');
+const server = https.createServer({pfx});
+server.on('request',(req,res)=>{
+	let body = '';
+	req.on('data', chunk => {
+		body += chunk.toString(); // convert Buffer to string
+	});
+	req.on('end', () => {
+		const rep = JSON.parse(body).repository;
+		switch(rep.name) {
+			case 'Discord-Selfbot':
+				exec(`cd /home/pi/bin/selfbot && git pull ${body.repository.clone_url} master`);
+				break;
+
+			default:
+				console.error(`${rep.name} not recogonized`);
+		}
+		console.log(`${rep.name} updated`);
+		res.end('ok');
+	});
+})
+server.listen(5555);
