5fcb32af856160c2e502b977e08901e0059ba998
unknown <Robokatbot@gmail.com>
2020-04-08 11:28:47 +0800

    application/json

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ function send(payload) {
 		path: "/api/webhooks/367102978403991554/Lk6DN4QksDhe4oUxOd0TWH-AYJGt7ZrUcSpgHzmQlgc57CgugSBuNts0qmFoCy1Cj2sz",
 		method: "POST",
 		headers: {
-			"Content-Type": "multipart/form-data"
+			"Content-Type": "application/json"
 		}
 	}).end(JSON.stringify(payload));
 }
